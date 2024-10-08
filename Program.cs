b76062f2c4484f95bbdaaa0bce9cd22ee5e16726
dotNETDev-Zayniddin <dotnetdev2000@gmail.com>
2024-10-09 00:13:25 +0500

    Takrorlash: Task submission

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿/*1 dan 1000 gacha bo’lgan toq sonlarni yig’indisini ekranga chiqaradigan dastur tuzing.
+ While loop dan foydalaning!
+*/
+using System;
+namespace Takrorlash
+{
+    class Program
+    {
+        static void Main ()
+        {
+            int result = 0;
+            int i = 0;
+            while(i < 1000)
+            {
+                if(i % 2 == 1){
+                    result += i;
+                    i++;       
+                }
+                else
+                {
+                    i++;
+                }
+            }   
+            Console.WriteLine ($"Natija: {result}");
+            
+        }
+    }
+}
